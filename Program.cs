f691d7ba84c73447d039d5aa911f544c8977be7c
Amirali shahi <amiralishahi0675@gmail.com>
2025-05-07 02:17:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿/*input radius of the circle
+Console.Write("Enter the radius in meter: ");
+int radius = int.Parse(Console.ReadLine());
+
+//calculate circle environment
+double env = 2 * Math.PI * radius;
+
+//display circle environment
+Console.WriteLine("Circle environment is: " + env + " meters");*/
+
+
+/*// input tile width
+Console.Write("Enter the width of each tile in meter: ");
+double tileWidth = double.Parse(Console.ReadLine());
+
+// calculate number of tiles needed
+double tileCount = Math.Ceiling(env / tileWidth);
+
+// display result
+Console.WriteLine("Number of tiles needed: " + tileCount);*/
+
+
+/*// input length
+Console.Write("Enter the length in meter: ");
+double length = double.Parse(Console.ReadLine());
+
+// input width
+Console.Write("Enter the width in meter: ");
+double width = double.Parse(Console.ReadLine());
+
+// calculate area
+double area = length * width;
+
+// display area
+Console.WriteLine("Area of the rectangle is: " + area + " square meters");*/
+
+/*// input tile length
+Console.Write("Enter the tile length in meter: ");
+double tileLength = double.Parse(Console.ReadLine());
+
+// input tile width
+Console.Write("Enter the tile width in meter: ");
+double tileWidth2 = double.Parse(Console.ReadLine());
+
+// calculate area of one tile
+double tileArea = tileLength * tileWidth2;
+
+// calculate number of tiles needed
+double tileCount2 = Math.Ceiling(area / tileArea);
+
+// display result
+Console.WriteLine("Number of floor tiles needed: " + tileCount2);*/
+
+
+/*// calculate uncovered area
+double uncoveredArea = area - (circleArea + (tileCount2 * tileArea));
+
+// display the result
+Console.WriteLine("Uncovered area: " + uncoveredArea + " square meters");*/
+
+
+
+
